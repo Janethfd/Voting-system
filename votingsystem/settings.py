b4677e8aaec260c22dcb57551b8ac9487032dea7
ezinne <janethnath58@gmail.com>
2023-03-18 09:55:12 -0700

    modified changes in requirements.txt and setings

--- votingsystem/settings.py
+++ votingsystem/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 from decouple import config
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "crispy_forms",
-    'crispy_bootstrap5'
+    'crispy_bootstrap5',
+
 ]
 
 MIDDLEWARE = [
@@ -129,7 +129,15 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
